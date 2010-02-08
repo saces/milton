2c0644933ee1166a776a3ecbb51bb36b102b4862
alex <alex@89edebda-9267-40c1-a934-9420f7305bf7>
2010-02-08 02:12:26 +0000

    Added CalDavPropertySource and CardDavPropertySource skeletons. PropertyWriters to follow shortly

---
+++ milton/milton-caldav/src/main/java/com/ettrema/http/caldav/CalDavPropertySource.java
@@ -0,0 +1,18 @@
+package com.ettrema.http.caldav;
+
+import com.bradmcevoy.http.webdav.AbstractPropertySource;
+import com.bradmcevoy.property.PropertySource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author alex
+ */
+public class CalDavPropertySource extends AbstractPropertySource {
+
+  private static final Logger log = LoggerFactory.getLogger( CalDavPropertySource.class );
+
+
+
+}
---
+++ milton/milton-caldav/src/main/java/com/ettrema/http/carddav/CardDavPropertySource.java
@@ -0,0 +1,15 @@
+package com.ettrema.http.carddav;
+
+import com.bradmcevoy.http.webdav.AbstractPropertySource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author alex
+ */
+public class CardDavPropertySource extends AbstractPropertySource {
+
+  private static final Logger log = LoggerFactory.getLogger( CardDavPropertySource.class );
+
+}
